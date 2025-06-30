d385951356501deb38c069f8e2c7ea47b2657b53
shamoo53 <aliyushamsu16@gmail.com>
2025-06-30 15:33:54 +0100

    implementation of Database-Confguration-and-TypeORM-Setup

--- src/Db/typeorm.config.ts
+++ src/Db/typeorm.config.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
-import { dataSourceOptions } from './src/config/database.config';
+import { dataSourceOptions } from '../config/database.config';
 
 config();
 
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,20 +1,32 @@
-import { DataSource } from "typeorm"
-import { ConfigService } from "@nestjs/config"
-import { config } from "dotenv"
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
 
-config()
+config();
 
-const configService = new ConfigService()
+const configService = new ConfigService();
 
-export default new DataSource({
-  type: "postgres",
-  host: configService.get("DB_HOST", "localhost"),
-  port: configService.get("DB_PORT", 5432),
-  username: configService.get("DB_USERNAME", "nestuser"),
-  password: configService.get("DB_PASSWORD", "nestpassword"),
-  database: configService.get("DB_NAME", "nestdb"),
-  entities: ["src/**/*.entity{.ts,.js}"],
-  migrations: ["src/migrations/*{.ts,.js}"],
-  synchronize: configService.get("NODE_ENV") !== "production",
-  logging: configService.get("NODE_ENV") === "development",
-})
+const isTest = configService.get('NODE_ENV') === 'test';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get('DB_HOST', 'localhost'),
+  port: configService.get('DB_PORT', 5432),
+  username: configService.get('DB_USERNAME', 'nestuser'),
+  password: configService.get('DB_PASSWORD', 'nestpassword'),
+  database: isTest
+    ? configService.get('DB_TEST_NAME', 'nestdb_test')
+    : configService.get('DB_NAME', 'nestdb'),
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  synchronize: configService.get('NODE_ENV') !== 'production',
+  logging: configService.get('NODE_ENV') === 'development',
+  extra: {
+    max: 20, // max pool size
+    min: 10, // min pool size
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  },
+};
+
+export default new DataSource(dataSourceOptions);
